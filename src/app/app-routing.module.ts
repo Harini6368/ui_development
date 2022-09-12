e65a65be1726050fdb786b36e952dee86226a9e3
Harinipriyadharshini P <harinipriyadharshini@SCIAD39LP232.local>
2022-09-12 18:13:25 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HeaderComponent},
+{path:'main',component:MainpageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
